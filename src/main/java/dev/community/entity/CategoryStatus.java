1c8b45e7bca399c45e1e8fa2344abda583f23f84
한인혁 <138432939+R4mel@users.noreply.github.com>
2025-07-07 15:17:37 +0900

    entity

--- src/main/java/dev/community/entity/CategoryStatus.java
+++ src/main/java/dev/community/entity/CategoryStatus.java
@@ -1,4 +1,18 @@
 package dev.community.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum CategoryStatus {
+    FREE(0L, "자유게시판"),
+    TIP(1L, "팁게시판"),
+    QUESTION(2L, "질문게시판");
+
+    private final Long code;
+    private final String description;
+
+    CategoryStatus(Long code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 }
